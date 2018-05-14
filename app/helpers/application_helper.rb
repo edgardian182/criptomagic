988d57ad044ad3437bc6d80ae371bba62b1b4c02
Edgar Quintana <edgarquintana984@hotmail.com>
2018-05-14 12:24:37 -0500

    Adds FLASH NOTICES alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    {
+      success: "alert-success",
+      error: "alert-danger",
+      notice: "alert-info",
+      warning: "alert-warning"
+    }[flash_type.to_sym] || flash_type.to_s
+  end
+
+  def bootstrap_glyphs_icon(flash_type)
+    {
+      success: "glyphicon-ok",
+      error: "glyphicon-exclamation-sign",
+      notice: "glyphicon-info-sign",
+      warning: "glyphicon-warning-sign"
+    }[flash_type.to_sym] || 'glyphicon-screenshot'
+  end
 end
